54169796933bd4b3eb98eaba818c28e009714539
bhagatmayuri2027 <bhagatmayuri2027@gmail.com>
2026-07-15 22:00:37 +0530

    Update app.py

--- app.py
+++ app.py
@@ -14,8 +14,8 @@ canvas_result=st_canvas(
     stroke_color="#FFFFFFFF",
     stroke_width=10,
     background_color="#000000",
-    width=100,
-    height=100,
+    width=400,
+    height=400,
     drawing_mode="freedraw",
     key="canvas"
 )
